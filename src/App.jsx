f7be0b6cbc8db3a4a42f40ca2a876c0b584e8fdf
pushpendu <pushpendu.ghosh@i2itherapy.com>
2023-09-01 01:11:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Meghna Pandey here is the material Ui with buttons</h1>
+        <Button variant="contained" color="primary">
+          Meghna 
+        </Button>
+        <Button variant="contained" color="secondary">
+          Pandey
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
